0695ec4ebf3e4d1534d6e9ab5312bd8bab345a5f
MBalciunas <33152745+MBalciunas@users.noreply.github.com>
2017-10-27 16:28:42 +0300

    Add project files.

---
+++ FibonacciDP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace FibonacciDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for fibonacci sequence");
+            var n = Convert.ToUInt64(Console.ReadLine());
+            ulong[] memo = new ulong[n];
+            Console.WriteLine(Fibonacci(n, memo));
+        }
+
+        static ulong Fibonacci(ulong n, ulong[] memo)
+        {
+            if (n <= 0)
+                return 0;
+            else if (n <= 2)
+                return 1;
+
+            else if (memo[n - 1] == 0)
+                memo[n - 1] = Fibonacci(n - 1, memo) + Fibonacci(n - 2, memo);
+
+            return memo[n - 1];
+        }
+    }
+}
